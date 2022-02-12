8be414b0af7c10130beded1595ee4462371064be
Markos Galikas <91387614+Markos-G@users.noreply.github.com>
2022-02-12 13:25:06 +0200

    Update app.py

--- Sudoku/app.py
+++ Sudoku/app.py
@@ -3,75 +3,90 @@ import os
 import mysql.connector
 from mysql.connector import Error
 
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 
 from random import randint
 
 from db import create_connection ,fetch_query
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    # app.config["TEMPLATES_AUTO_RELOAD"] = True
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/')
-    def home():
-
-        return render_template("home.html",n=0)
-
-    @app.route('/newgame')
-    def newgame():
-
-        config={}
-        with open('config.txt','r') as f:
-            config['host_name'] = f.readline().strip()
-            config['user_name'] = f.readline().strip()
-            config['user_password'] = f.readline().strip()
-            config['database'] = f.readline().strip()
-
-        connection = create_connection(**config)
-
-        puzzle = fetch_query(connection, one=randint(0, 1_000_000))
-        print((puzzle))
-        grid = [ [0 for i in range(9)] for j in range(9)]
-
-        k = 0
-        for i,row in enumerate(grid):
-            for j,col in enumerate(row):
-                if puzzle[k] == '0':
-                    grid[i][j] = ''
-                else:
-                    grid[i][j] = puzzle[k]
-                k += 1
-        
-        for row in grid:
-            print(row,end='\n')
-        connection.close()
-        return render_template("newgame.html",n=grid)
-
-    @app.route('/answer', methods=["POST"])
-    def answer():
-
-        return render_template("answer.html")
+from sudoku import fill_matrix,solve
 
 
+# create and configure the app
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# a simple page that says hello
+@app.route('/')
+def home():
+    return render_template("home.html",n=0)
 
 
-    return app
+@app.route('/newgame')
+def newgame():
+
+    config={}
+    with open('config.txt','r') as f:
+        config['host_name'] = f.readline().strip()
+        config['user_name'] = f.readline().strip()
+        config['user_password'] = f.readline().strip()
+        config['database'] = f.readline().strip()
+    connection = create_connection(**config)
+    
+    puzzle = fetch_query(connection, one=randint(0, 1_000_000))
+    grid = [ [0 for i in range(9)] for j in range(9)]
+    k = 0
+    for i,row in enumerate(grid):
+        for j,col in enumerate(row):
+            if puzzle[k] == '0':
+                grid[i][j] = '_'
+            else:
+                grid[i][j] = puzzle[k]
+            k += 1
+
+    session['grid'] = grid
+
+    # for row in grid:
+    #     print(row,end='\n')
+
+    ids = [x for x in range(81)]
+
+    connection.close()
+    return render_template("newgame.html",n=grid, ids=ids)
+    
+
+@app.route('/submit', methods=['post'])
+def submit():
+
+    grid = session['grid']
+    grid = [[int(col) if col !='_' else 0 for col in row] for row in grid]
+
+    X,Y = fill_matrix(grid)
+    solution = solve(X, Y, sol=[])
+    for sol in solution:
+        grid[sol[0]][sol[1]] = sol[2]
+    # for row in grid:
+    #     print(row,end='\n')
+
+    inpts={}
+    for cell in range(81):
+        number = request.form.get(str(cell))
+        if number is not None:
+            inpts[cell] = number
+    # print (inpts)      
+    user_grid = session['grid']
+    for key,value in inpts.items():
+        row,col = divmod(key,9)
+        user_grid[row][col] = value
+    # print (user_grid)
+    err=[]
+    for key,value in inpts.items():
+        row,col = divmod(key,9)
+        if value != str(grid[row][col]):
+            err.append(key)
+    print (err)
+
+    ids = [x for x in range(81)]
+    return render_template("submit.html", n=user_grid, ids=ids ,err=err)
