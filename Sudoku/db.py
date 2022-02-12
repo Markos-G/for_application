8c95ad481c98430d1d75f9682877fb8d408cb97e
Markos Galikas <91387614+Markos-G@users.noreply.github.com>
2022-02-12 13:36:09 +0200

    Update db.py

--- Sudoku/db.py
+++ Sudoku/db.py
@@ -1,13 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
-import csv
 
-# config={}
-# with open('config.txt','r') as f:
-# 	config['host_name'] = f.readline().strip()
-# 	config['user_name'] = f.readline().strip()
-# 	config['user_password'] = f.readline().strip()
-# config['database'] = ''
 
 def create_connection(host_name, user_name, user_password, database):
 
@@ -25,7 +18,6 @@ def create_connection(host_name, user_name, user_password, database):
 
     return connection
 
-# connection = create_connection(**config)
 
 def create_database(connection, db):
     query = 'CREATE SCHEMA IF NOT EXISTS {}'.format(db)
@@ -37,10 +29,6 @@ def create_database(connection, db):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-# db='sudoku'
-# create_database(connection,db)
-# config['database']=db
-# connection = create_connection(**config)
 
 def use_database(connection, db):
     query = 'USE {}'.format(db)
@@ -52,7 +40,7 @@ def use_database(connection, db):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-# use_database(connection,db)
+
 
 def execute_query(connection, query, action=''):
 
@@ -64,16 +52,6 @@ def execute_query(connection, query, action=''):
         cursor.close()
     except Error as e:
         print(f"The error '{e}' occurred")
-
-# create_table = """
-	# CREATE TABLE generator (
-	# 	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	# 	puzzle CHAR(81),
-	# 	solution CHAR(81),
-	# 	PRIMARY KEY  (id)
-	# ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"""
-
-# execute_query(connection, create_table, action='create')
 
 
 def execute_many_query(connection, query, args, action=''):
@@ -87,31 +65,6 @@ def execute_many_query(connection, query, args, action=''):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-
-# fill_table ="""
-#   INSERT INTO generator (puzzle, solution)
-#   VALUES (%s, %s)
-# """
-
-# with open('sudoku.csv', newline='') as csvfile:
-#     print ('csv file opened') 
-#     ins_vals=[]
-#     reader = csv.DictReader(csvfile)
-#     count = 0
-#     step = 20000
-#     stop = 1_000_000//step + 1
-#     lin_space = [k*step for k in range(1,stop)]
-#     while reader:
-#         count +=1
-#         try:
-#             row = next(reader)
-#         except:
-#             break
-#         ins_vals.append( (row['quizzes'], row['solutions']) )
-#         if count in lin_space:
-#             execute_many_query(connection, fill_table, ins_vals, action='insert into' )
-#             ins_vals = []
-#     print ('csv file closed')
 
 
 def fetch_query(connection, one=False, many=0, all=False):
@@ -135,6 +88,3 @@ def fetch_query(connection, one=False, many=0, all=False):
         return row
     except Error as e:
         print(f"The error '{e}' occurred")
-
-
-# connection.close()
