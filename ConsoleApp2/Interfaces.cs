cbfbd853546e0ade9b26651a4603b4aada5d3a2f
Markos Galikas <91387614+Markos-G@users.noreply.github.com>
2023-03-15 12:33:51 +0000

    Add files via upload

---
+++ ConsoleApp2/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2;
+interface IHotelManager
+{
+    bool AddRoom(Room room);
+    bool DeleteRoom(int roomNumber);
+    void ListRooms();
+    void ListRoomsOrderedByPrice();
+    void GenerateReport(string fileName);
+}
+
+interface IHotelCustomer
+{
+    void ListAvailableRooms(Booking wantedBooking, Size roomSize);
+    void ListAvailableRooms(Booking wantedBooking, Size roomSize, int maxPrice);
+    bool BookRoom(int roomNumber, Booking wantedBooking);
+}
+
+interface IOverlappable
+{
+    bool Overlaps(Booking other);
+}
