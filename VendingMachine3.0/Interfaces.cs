63a0aa3e5e4ee270987a721c1bdcddc662ff7e22
Markos Galikas <91387614+Markos-G@users.noreply.github.com>
2023-05-31 18:41:48 +0100

    Add files via upload

---
+++ VendingMachine3.0/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿namespace VendingMachine;
+
+interface IVendingMachine
+{
+    bool GiveChange(double difference, Dictionary<double, int> pool, int[] cashInserted);
+    void ChangePrices(List<Snack> snacks);
+    void AddChange(Dictionary<double, int> pool);
+    void GenerateReport(Dictionary<double, int> pool);
+
+}
+
+
---
+++ VendingMachine3.0/Snack.cs
@@ -0,0 +1,27 @@
+﻿namespace VendingMachine
+{
+    internal class Snack
+    {
+
+        private string _label;
+        public string label { get { return _label; } set { _label = value; } }
+
+        private int _quantity;
+        public int quantity { get { return _quantity; } set { _quantity = value; } }
+        private double _price;
+        public double price { get { return _price; } set { _price = value; } }
+
+
+
+
+        public Snack(string label, int quantity, double price)
+        {
+            this.label = label;
+            this.quantity = quantity;
+            this.price = price;
+
+        }
+
+    }
+} 
+     
